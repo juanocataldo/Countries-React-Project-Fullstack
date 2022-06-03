57720da3ac6aa5bd0b0ab1d7527a092e039a5400
Juan Manuel Cataldo <juanocataldo@gmail.com>
2022-06-03 08:23:21 -0300

    ok

--- client/src/Components/Country.jsx
+++ client/src/Components/Country.jsx
@@ -3,7 +3,8 @@ import { NavLink } from 'react-router-dom'
 import '../Styles/country.css'
 import { getCountries, GET_ALL_COUNTRIES, GET_FULL_COUNTRY_LIST, setFavCountry, SET_FAVORITE_COUNTRY } from '../Redux/actions'
 import { useEffect, useState } from 'react'
-
+import heart_red from '../Assets/heart_red.png'
+import heart_black from '../Assets/heart_black.png'
 
 export function Country({ id, name, flag, continent, favorite }) {
     const dispatch = useDispatch()
@@ -42,8 +43,10 @@ export function Country({ id, name, flag, continent, favorite }) {
     }
 
     return <div className="country">
-        {favorite && <svg onClick={() => setAsFavorite()} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z" /></svg>}
-        {favorite || <svg onClick={() => setAsFavorite()} xmlns="http://www.w3.org/2000/svg" color='white' width="24" height="24" viewBox="0 0 24 24"><path d="M12 9.229c.234-1.12 1.547-6.229 5.382-6.229 2.22 0 4.618 1.551 4.618 5.003 0 3.907-3.627 8.47-10 12.629-6.373-4.159-10-8.722-10-12.629 0-3.484 2.369-5.005 4.577-5.005 3.923 0 5.145 5.126 5.423 6.231zm-12-1.226c0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-7.962-9.648-9.028-12-3.737-2.338-5.262-12-4.27-12 3.737z" /></svg>}
+        
+        
+        {favorite && <img src={heart_red} alt="" id='heart' onClick={() => setAsFavorite()}/>}
+        {favorite || <img src={heart_black} alt="" id='heart' onClick={() => setAsFavorite()}/>}
 
         <NavLink to={`/details/${id}`} className="countryAccess">
             <div className="country-content">
